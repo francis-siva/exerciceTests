b043cd5b6c63775207d93a4e7cbad047b91ed9c8
unknown <fsivasithamparam@gmail.com>
2019-02-22 15:46:59 +0100

    4Tests passing OK req(GET,POST) check book (status=200&length=0)

--- src/routes/books.js
+++ src/routes/books.js
@@ -28,39 +28,39 @@ const initialStructure = {
 };
 
 const postBook = (req, res) => {
-  res.status(200).send({ message: 'route non activé' });
+  // res.status(200).send({ message: 'route non activé' });
 
-  // const pathBooks = path.join(__dirname, '../data/books.json');
-  // initIfFileIfMissing(pathBooks, initialStructure);
-  // fs.readFile(pathBooks, 'utf8', (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     res.status(400).send({ message: 'an Error occured' });
-  //   } else {
-  //     console.log(data);
-  //     let obj;
-  //     if (!data) {
-  //       obj = obj = { books: [] };
-  //     } else {
-  //       obj = JSON.parse(data); //now it an object
-  //     }
-  //     if (!obj || !obj.books) obj = { books: [] };
-  //     obj.books.push({
-  //       id: uuid(),
-  //       title: req.body.title,
-  //       years: req.body.years,
-  //       pages: req.body.pages
-  //     }); //add some data
-  //     const json = JSON.stringify(obj); //convert it back to json
-  //     fs.writeFile(pathBooks, json, 'utf8', (err, data) => {
-  //       if (err) {
-  //         res.status(400).send({ message: 'error adding the book' });
-  //       } else {
-  //         res.status(200).send({ message: 'book successfully added' });
-  //       }
-  //     });
-  //   }
-  // });
+  const pathBooks = path.join(__dirname, '../data/books.json');
+  initIfFileIfMissing(pathBooks, initialStructure);
+  fs.readFile(pathBooks, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send({ message: 'an Error occured' });
+    } else {
+      console.log(data);
+      let obj;
+      if (!data) {
+        obj = obj = { books: [] };
+      } else {
+        obj = JSON.parse(data); //now it an object
+      }
+      if (!obj || !obj.books) obj = { books: [] };
+      obj.books.push({
+        id: uuid(),
+        title: req.body.title,
+        years: req.body.years,
+        pages: req.body.pages
+      }); //add some data
+      const json = JSON.stringify(obj); //convert it back to json
+      fs.writeFile(pathBooks, json, 'utf8', (err, data) => {
+        if (err) {
+          res.status(400).send({ message: 'error adding the book' });
+        } else {
+          res.status(200).send({ message: 'book successfully added' });
+        }
+      });
+    }
+  });
 };
 
 /*
--- src/test/book.js
+++ src/test/book.js
@@ -8,6 +8,9 @@ import nock from 'nock';
 import server from '../server';
 import resetDatabase from '../utils/resetDatabase';
 
+// let assert = require('assert');
+let assert = require('chai').assert;
+
 chai.use(chaiHttp);
 chai.use(chaiNock);
 chai.use(chaiAsPromised);
@@ -15,9 +18,21 @@ chai.use(chaiAsPromised);
 // tout les packages et fonction nescessaire au test sont importé ici, bon courage
 
 // fait les Tests d'integration en premier
+// const _initialStructure = {
+//     books: []
+// };
 
 describe('Test sur une requête GET /book', function() {
-    it('should return code 200', () => {
+    let initialStructure = {
+        books: []
+    };
+    beforeEach((done) => {
+        resetDatabase(path.join(__dirname,'../data/books.json'), initialStructure);
+        done();
+    });
+
+    it('should return code 200', done => {
+
         chai
             .request(server)
             .get('/book')
@@ -26,7 +41,75 @@ describe('Test sur une requête GET /book', function() {
                 expect(res).to.have.status(200);
                 expect(res.body).to.be.a('object');
                 expect(res.body.books).to.be.a('array');
+                done();
+            });
+    })
+
+    it('should return res.body.books.length', done => {
+
+        chai
+            .request(server)
+            .get('/book')
+            .end((err, res) => {
+                if (err) console.log(err); //console.log('VaLuE ::: ' + res.body.books.length);
                 expect(res.body.books.length).to.equal(0);
+                done();
+            });
+    })
+
+    /*
+    it('should be an object Tested by Assert module', done => {
+        
+        beforeEach(() => {
+            resetDatabase(path.basename('./../data/books.json', initialStructure));
+        });
+
+        chai
+            .request(server)
+            .get('/book')
+            .end((err, res) => {
+                if (err) console.log(err);
+
+                // console.log('2nd Test => ' + chai.request(server).get('/book'));
+
+                const testContent = chai.request(server).get('/book');
+                assert.equal(testContent, 'object');
+                done();
+            });       
+    })*/
+});
+
+describe('Test sur une requête POST /book', function () {
+    
+    let initialStructure = {
+        books: []
+    };
+
+    beforeEach((done) => {
+        resetDatabase(path.join('./../data/books.json'), initialStructure);
+        done();
+    });
+
+    it('should return code 200', done => {
+        chai
+            .request(server)
+            .post('/book')
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res).to.have.status(200);                
+                done();
+            });
+    })
+
+    it('should return key message => \"book successfully added\"', done => {
+        chai
+            .request(server)
+            .post('/book')
+            .end((err, res) => {
+                console.log("Here .. ");
+                if (err) console.log(err);
+                console.log("Here .. ", res.body);
+                expect(res.body.message).to.equal("book successfully added");                
                 done();
             });
     })
